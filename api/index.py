b8fc7859c00040072f4344b1ce1a2b4107374e47
Pedro Sebastián Degaudenci <73450522+pdegaudenci@users.noreply.github.com>
2025-07-12 23:20:32 +0000

    mangum handler added

--- api/index.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from mangum import Mangum
-from api.webhook import router  # Asegúrate de que esté bien importado
-
-app = FastAPI()
-app.include_router(router)
-
-# Esto es lo que Vercel necesita
-handler = Mangum(app)
